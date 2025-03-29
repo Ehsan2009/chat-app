33fdedafbe11db2ab31b03dbbc17ebf7d986fe17
Ehsan Javdan <ehsanjavdan77@gmail.com>
2025-03-29 06:24:18 +0400

    enabled path Url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  usePathUrlStrategy();
+
   await Hive.initFlutter();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
