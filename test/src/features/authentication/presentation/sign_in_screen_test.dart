3cbf219b2178af80831b12f985c0695ee47b1474
Ehsan Javdan <ehsanjavdan77@gmail.com>
2025-04-14 19:44:21 +0400

    i forgot to save the sign in screen, so now i push the tested sign in screen file

--- test/src/features/authentication/presentation/sign_in_screen_test.dart
+++ test/src/features/authentication/presentation/sign_in_screen_test.dart
@@ -1,7 +1,72 @@
+import 'package:chat_app/src/common_widgets/custom_text_form_field.dart';
+import 'package:chat_app/src/features/authentication/data/auth_repository.dart';
+import 'package:chat_app/src/features/authentication/presentation/sign_in_screen.dart';
+import 'package:chat_app/src/features/authentication/presentation/widgets/auth_submit_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../mocks.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (_, __) => SignInScreen()),
+    GoRoute(
+      name: 'chat',
+      path: '/chat',
+      builder: (_, __) => const SizedBox(), // dummy placeholder
+    ),
+  ],
+);
 
 void main() {
-  testWidgets('sign in screen ...', (tester) async {
-    // TODO: Implement test
+  final testEmail = 'test@example.com';
+  final testPassword = '12345678';
+
+  late MockAuthRepository mockAuthRepository;
+  setUp(() {
+    mockAuthRepository = MockAuthRepository();
+  });
+
+  group('register screen test', () {
+    testWidgets(
+      'when filling inputs and tapping Login button, authRepository.signInWithEmailAndPassword() should be called',
+      (tester) async {
+        when(
+          () => mockAuthRepository.signInWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          ),
+        ).thenAnswer((_) async => Future.value());
+
+        await tester.pumpWidget(
+          ProviderScope(
+            overrides: [
+              authRepositoryProvider.overrideWithValue(mockAuthRepository),
+            ],
+            child: MaterialApp.router(routerConfig: router),
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        final emailField = find.byType(CustomTextFormField).first;
+        final passwordField = find.byType(CustomTextFormField).at(1);
+
+        await tester.enterText(emailField, testEmail);
+        await tester.enterText(passwordField, testPassword);
+        await tester.tap(find.byType(AuthSubmitButton));
+        await tester.pumpAndSettle();
+
+        verify(
+          () => mockAuthRepository.signInWithEmailAndPassword(
+            testEmail,
+            testPassword,
+          ),
+        ).called(1);
+      },
+    );
   });
 }
